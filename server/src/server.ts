99b095d8c1dc5dbe9e3a4754c0ec39b13bb68203
carloscastrofilho <cpcftq@gmail.com>
2023-06-16 10:49:07 -0300

    fechamento aula 4- ok

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,8 +5,18 @@ import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import multipart from '@fastify/multipart'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true, // todas a url de front end pode acessa o back
@@ -18,6 +28,7 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
